43a63c6739f588e72ea33e29f0f21ce2509a2879
Xemn <spysschaert.pro@gmail.com>
2024-03-13 15:08:30 +0100

    Get the id params with url.searchparams and get methods, Create a function named getPhotographersById who fetch the json data file and get the information of photographer who take the id value of a photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,17 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const params = new URL(document.location).searchParams;
+// console.log(params);
+const id = params.get("id");
+// console.log(id);
+
+async function getPhotographersById() {
+	const response = await fetch("../data/photographers.json");
+	const datas = await response.json();
+	console.log(datas);
+	const currentPhotographer = datas.photographers.find(
+		(photographer) => photographer.id == id
+	);
+	console.log(currentPhotographer);
+}
+
+getPhotographersById();
