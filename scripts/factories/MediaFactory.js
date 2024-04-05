931827ac44fbbf1c8843d039203c2795614c1529
Xemn <spysschaert.pro@gmail.com>
2024-04-05 16:06:04 +0200

    Change the method by a constructor

--- scripts/factories/MediaFactory.js
+++ scripts/factories/MediaFactory.js
@@ -1,14 +1,12 @@
-// Factory permettant d'instancier la classe nécéssaire :
 class MediaFactory {
-	// Méthode permettant de créer un objet media en fonction de son type :
-	createMedia(type, data) {
-		switch (type) {
-			case "image":
-				return new Image(data);
-			case "video":
-				return new Video(data);
-			default:
-				throw new Error("Type de média non reconnu");
+	// Constructeur permettant de créer le bon objet media en fonction de son type :
+	constructor(data, name) {
+		if (data.image) {
+			return new Image(data, name);
+		} else if (data.video) {
+			return new Video(data, name);
+		} else {
+			throw new Error("Type de média non reconnu");
 		}
 	}
 }
