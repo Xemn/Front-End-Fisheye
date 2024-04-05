bcee3b10bc6c471072a3f99212aca302b144466e
Xemn <spysschaert.pro@gmail.com>
2024-04-05 16:20:17 +0200

    Get the value of the h1 from the page - Get the h2 from the header of the modal - Put the value from the h1 in the modal header in the html

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+	const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+	const photographerName = document.querySelector("h1").innerText;
+	const modalHeader = document.querySelector(".modal header h2");
+	modalHeader.innerHTML += `<br> ${photographerName}`;
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+	const modal = document.getElementById("contact_modal");
+	modal.style.display = "none";
 }
