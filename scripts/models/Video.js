9a90fb6e3c43df5cc59d5b58858e244d59f37cd8
Xemn <spysschaert.pro@gmail.com>
2024-04-05 16:15:29 +0200

    Remove the getter type and this.type and create a new constante named path for the name of the file

--- scripts/models/Video.js
+++ scripts/models/Video.js
@@ -1,18 +1,13 @@
 // Classe pour constuire une Video :
 class Video extends Media {
-	constructor(data) {
+	constructor(data, name) {
 		// Hérite des propriétés de la classe Media :
 		super(data);
-		this._type = "video";
-		this._src = `assets/photographerGallery/${data.video}`;
+		const path = name.replace(/\s/g, "_");
+		this._src = `assets/photographerGallery/${path}/${data.video}`;
 	}
 
 	// Getters :
-
-	get type() {
-		return this._type;
-	}
-
 	get src() {
 		return this._src;
 	}
