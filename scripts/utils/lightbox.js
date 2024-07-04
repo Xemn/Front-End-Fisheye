ed7b8c3f2b2ae4b512136f0d88c7f82366d08228
Xemn <spysschaert.pro@gmail.com>
2024-07-04 17:06:01 +0200

    Modify the displayLightbox function and closeLightbox function in order to take the utils handleKeyPress function

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,12 +1,30 @@
+let keyPressHandler = null;
+
 function displayLightbox(photographerMedias, index) {
 	const lightboxModal = lightboxDom(photographerMedias, index);
 	lightboxModal.style.display = "flex";
+
+	// Supprimer l'ancien écouteur s'il existe
+	if (keyPressHandler) {
+		document.removeEventListener("keydown", keyPressHandler);
+	}
+
+	// Définir une nouvelle fonction comme gestionnaire
+	keyPressHandler = (event) => handleKeyPress(event, photographerMedias, index);
+
+	// Ajouter l'écouteur
+	document.addEventListener("keydown", keyPressHandler);
 }
 
 function closeLightbox() {
 	const lightboxWrapper = document.getElementById("lightbox_wrapper");
 	if (lightboxWrapper) {
 		lightboxWrapper.style.display = "none";
+		// Supprimer l'écouteur d'événement pour les touches
+		if (keyPressHandler) {
+			document.removeEventListener("keydown", keyPressHandler);
+			keyPressHandler = null;
+		}
 	}
 }
 
