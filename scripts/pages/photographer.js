58ed44185132ec3cd8b8f934cb13c0eed67d1828
Xemn <spysschaert.pro@gmail.com>
2024-07-21 15:04:43 +0200

    Remove the API calls in order to put them in a seperate files

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,30 +1,4 @@
 //Mettre le code JavaScript lié à la page photographer.html
-const params = new URL(document.location).searchParams;
-// console.log(params);
-const id = params.get("id");
-// console.log(id);
-async function getDatasPhotographersById() {
-	const response = await fetch("./data/photographers.json");
-	const datas = await response.json();
-	// console.log(datas);
-	/* Récupération des informations concernant le photographe
-	possédant l'id présent dans l'URL : */
-	const currentPhotographer = datas.photographers.find(
-		(photographer) => photographer.id == id
-	);
-	console.log(currentPhotographer);
-	// Récupération des médias :
-	const medias = datas.media;
-	// console.log(medias);
-	// Récupération des médias concernant le dit photographe :
-	const photographerMedias = medias
-		.map((media) => new MediaFactory(media, currentPhotographer.name))
-		.filter((media) => media.photographerId == currentPhotographer.id);
-	console.log(photographerMedias);
-
-	return { currentPhotographer, photographerMedias };
-}
-
 function displayData(photographer, photographerMedias) {
 	const photographerHeader = document.querySelector(".photograph-header");
 	const photographerModel = photographerTemplate(photographer);
